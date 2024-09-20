be231276e2be9998feebef6079821de6651bca6d
agusev2311 <agusev2311@mail.ru>
2024-09-20 11:34:34 +0300

    First step

--- main.py
+++ main.py
@@ -8,4 +8,21 @@ print(config)
 
 bot = telebot.TeleBot(config["tg-token"])
 
+register = dict([])
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, f"Здравcтвуйте! Сейчас вы будете проходить регистрацию. Пожалуйста введите своё <b>имя</b> и <b>фамилию</b> (<u>обязательно в таком порядке</u>)\n\nПозже администратор проверит, зарегестрированы ли вы, и в зависимости от этого вынесет вердикт: (зарегестрировать, проигнорировать (ваша заявка будет отклонена, но вы сможете подать ещё одну) или заблокировать (ваша заявка будет отклонена, и вы больше не сможете подать заявку))", parse_mode="HTML")
+    bot.register_next_step_handler(message, register_name)
+
+def register_name(message):
+    name = message.text.split()
+    print(name)
+    if len(name) != 2:
+        bot.reply_to(message, f"Вы ввели имя и фамилию неправильно. Введите их снова.")
+        bot.register_next_step_handler(message, register_name)
+    else:
+        register[message.from_user.id] = name
+        bot.reply_to(message, f"Мы отправили сообщение администратору. Теперь ожидайте подтверждения от него.")
+
 bot.polling(none_stop=True)
