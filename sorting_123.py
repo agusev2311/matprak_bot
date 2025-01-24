427a4f383c892043234586d7e17180d11c05a05a
Artem Gusev <agusev2311@mail.ru>
2025-01-24 22:58:21 +0300

    ))

--- sorting_123.py
+++ sorting_123.py
@@ -2,3 +2,4 @@ import sql_return
 
 def sort(list):
     return sorted(list, key=lambda x: (sql_return.get_user_name(int(x))[1], sql_return.get_user_name(int(x))[0]))
+    # нет, это не реальный алгоритм сортировки))я
