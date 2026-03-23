73693084e67f98bb211142af2ce10789513cf2ee
Artem Gusev <agusev2311@mail.ru>
2026-03-23 10:45:30 +0300

    start sh

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ python3 -m venv venv
 
 echo "Активация виртуального окружения..."
 source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "Запуск main.py..."
 python3 main.py
